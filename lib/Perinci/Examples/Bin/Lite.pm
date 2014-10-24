77dfbae95d5a36a8c12b233adc9277e96931e631
perlancar (@pc-office) <perlancar@gmail.com>
2014-10-24 11:54:19 +0700

    Initial commit

---
+++ lib/Perinci/Examples/Bin/Lite.pm
@@ -0,0 +1,23 @@
+package Perinci::Examples::Bin::Lite;
+
+# DATE
+# VERSION
+
+use 5.010001;
+
+1;
+# ABSTRACT: Perinci example CLI scripts (using Perinci::CmdLine::Lite)
+
+=head1 DESCRIPTION
+
+This distribution contains example scripts in C<bin/> which use
+L<Perinci::CmdLine::Lite>.
+
+
+=head1 SEE ALSO
+
+L<Perinci::Examples::Bin>
+
+L<Perinci::Examples::Bin::Any>
+
+=cut
